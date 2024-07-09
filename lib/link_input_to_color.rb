60f8460fc1eac46f5f7cb91a857e0dd3a3108512
weeezik <mkihanya.33@gmail.com>
2024-07-09 15:36:34 -0700

    Create method to convert numerical user input to color

---
+++ lib/link_input_to_color.rb
@@ -0,0 +1,18 @@
+def link_input_to_color user_number
+  case user_number
+  when "1"
+    :red
+  when "2"
+    :green
+  when "3"
+    :green
+  when "4"
+    :blue
+  when "5"
+    :cyan
+  when "6"
+    :magenta
+  else
+    puts "Invalid input"
+  end
+end
--- main.rb
+++ main.rb
@@ -3,15 +3,26 @@ require 'io/console'
 
 require_relative 'lib/computer'
 require_relative 'lib/list_colors'
+require_relative 'lib/link_input_to_color'
 
 computer = Computer.new
+# position_one = Position.new
+# position_two = Position.new
+# position_three = Position.new
+# position_four = Position.new
 
 guess_color_list = %i[red green yellow blue cyan magenta]
 feedback_color_list = %i[black white]
+board_array = ['_', '|', '_', '|', '_', '|', '_']
 
 list_colors guess_color_list, 1
-puts "\n"
+puts "\n #{board_array.join}"
+user_input = STDIN.getch
+puts link_input_to_color user_input
+
+
 list_colors feedback_color_list, 1
-puts ['.'.bold.blink, '|', ' ', '|', ' ', '|', ' '].join
 puts "Computer's secret code: #{computer.generate_secret_code guess_color_list}"
+
+
 
